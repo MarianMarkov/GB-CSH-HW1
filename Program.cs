0e63df3d13c727571d53553ae177071680edc7e7
Марьян Марков <alchnichort@gmail.com>
2022-12-24 15:50:12 +0300

    fc

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿int a, b, c;
+a = int.Parse(Console.ReadLine()!);
+b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine("Максимальное число:" + a);
+}
+else
+{
+    Console.WriteLine("Максимальное число:" + b);
+}
+
+a = int.Parse(Console.ReadLine()!);
+b = int.Parse(Console.ReadLine()!);
+c = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("Максимальное число:" + a);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число:" + c);
+    }
+}
+else
+{
+    Console.WriteLine("Максимальное число:" + b);
+}
+
+a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+a = int.Parse(Console.ReadLine()!);
+for (int i = 1; i < (a + 1); i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+}
